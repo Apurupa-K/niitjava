1512a460958c3846bfd5beaec772b265170dcb14
apurupa <apurupa@LAPTOP-SL8NQJ86>
2021-04-12 11:45:11 +0530

    constructor

---
+++ DXC niit/src/method/referance/constructor/ISample.java
@@ -0,0 +1,6 @@
+package method.referance.constructor;
+
+public interface ISample {
+	public Sample get(String string);
+
+}
---
+++ DXC niit/src/method/referance/constructor/Sample.java
@@ -0,0 +1,13 @@
+package method.referance.constructor;
+
+public class Sample {
+	public Sample() {
+		System.out.println("Sample constuctor called");
+
+	}
+	public Sample(String name) {
+
+		System.out.println("sample constructor called "+name);
+	}
+
+}
