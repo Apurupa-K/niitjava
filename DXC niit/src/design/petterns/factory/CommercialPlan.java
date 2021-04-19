e712c5078d36b613c5846b69786c8dcd24ffd5f4
apurupa <apurupa@LAPTOP-SL8NQJ86>
2021-04-19 11:04:51 +0530

    factory design patterns

---
+++ DXC niit/src/design/petterns/factory/CommercialPlan.java
@@ -0,0 +1,8 @@
+package design.petterns.factory;
+
+public class CommercialPlan extends Plan {
+	public void getRate(){   
+        rate=7.50;  
+   }  
+
+}
---
+++ DXC niit/src/design/petterns/factory/DomesticPlan.java
@@ -0,0 +1,8 @@
+package design.petterns.factory;
+
+public class DomesticPlan extends Plan {
+	public void getRate(){  
+        rate=3.50;              
+   }  
+
+}
